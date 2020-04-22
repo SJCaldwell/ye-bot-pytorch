e436a1f23ed816dccf2407a7f6bb483c11f6ab20
SJCaldwell <shane.caldwell12@ncf.edu>
2020-04-21 21:59:52 -0500

    Bar generator for training

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,14 +1,22 @@
 import torch
-
+import random
 def generate_vocab(filename):
     with open(filename) as vocab_filename:
         all_text = vocab_filename.read()
         unique_chars = set(all_text)
         unique_chars = sorted(list(unique_chars))
         unique_chars = ''.join(unique_chars)
-        return unique_chars, len(unique_chars)
+        return unique_chars, len(unique_chars), all_text
 
-all_letters, n_letters = generate_vocab('../data/all_merged.txt')
+def generate_bars(all_text):
+    bars = all_text.split('\n[')
+    return bars
+
+def generate_bar(): return '[' + random.choice(bars)
+
+all_letters, n_letters, all_text  = generate_vocab('../data/all_merged.txt')
+bars = generate_bars(all_text)
+
 
 def letter_to_index(letter):
     return all_letters.find(letter)
@@ -24,5 +32,10 @@ def line_to_tensor(line):
         tensor[li][0][letter_to_index(letter)] = 1
     return tensor
 
+def random_bar(text):
+    # from the text, pull a random bar
+    print('barrrr')
+
 if __name__ == "__main__":
     print(line_to_tensor("kanye west").shape)
+    print(generate_bar())
